441a87acfc884df986b1690211b91019f9060adf
Sunday Omolewu <omolewu12@gmail.com>
2023-05-09 02:44:20 -0700

    introto react using vite

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+function App(){
+  return(
+            <div>
+             <h2>Anyally Women's Summer Dressy</h2>
+             <div>
+              <img src="https://m.media-amazon.com/images/I/61NQoUmuKeL._AC_UY445_.jpg" alt="Anyally Women's Summer Dressy" />
+             </div>
+             <div>
+             Anyally Women's Summer Dressy Chiffon Blouses V Neck Short Sleeve Tunic Tops for Leggings Casual T-Shirts
+             </div>
+          </div>
+  )
+
+}
+
+
+
+export default App
